07d52a387ca144ba398733615aa31ec7b6b4e4f2
Nathan Ford <NathanFord68@gmail.com>
2023-11-08 11:09:05 -0700

    Initial commit - Project creation

---
+++ Source/TroubleInCognition/TroubleInCognitionGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TroubleInCognitionGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TROUBLEINCOGNITION_API ATroubleInCognitionGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
