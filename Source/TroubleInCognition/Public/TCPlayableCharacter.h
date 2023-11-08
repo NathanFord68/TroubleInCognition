93892bc8de129a21f45cb36b54f1be69366b238a
Nathan Ford <NathanFord68@gmail.com>
2023-11-08 11:50:51 -0700

    - Added player cpp class - Extended cpp to blueprint - Attacked a skelaton to the mesh for the character - Created spring arm and camera and set default location - Updated maps and modes to default to the prototype level

---
+++ Source/TroubleInCognition/Public/TCPlayableCharacter.h
@@ -0,0 +1,38 @@
+// Copyright 2023 Nathan Ford. All Right Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include <GameFramework/SpringArmComponent.h>
+#include <Camera/CameraComponent.h>
+#include "TCPlayableCharacter.generated.h"
+
+UCLASS()
+class TROUBLEINCOGNITION_API ATCPlayableCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ATCPlayableCharacter();
+
+protected:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	USpringArmComponent* SpringArmComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	UCameraComponent* CameraComponent;
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
