6cdd5eed2b70ae02718f429f1c7b381a03e641bc
Nathan Ford <NathanFord68@gmail.com>
2024-04-25 17:01:28 -0400

    Can interact with objects and call their interact method
    Can call action on world objects with equipped item
    
    Added animations for player
    Added animations for hatchet
    Synchronized animations for player and hatchet when equipped
    Refactored out the animation and player controller into components
    
    Got swing animation working

--- player/player.gd
+++ player/player.gd
@@ -2,20 +2,32 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 
+var equipped = null
+
+func _ready():
+	$AnimationPlayer.play("Idle")
+
 func _input(event):
-	if event is InputEventMouseButton and event.button_index == 1:
-		var click_normalized : Vector2 = position.direction_to(get_local_mouse_position())
-		var space = get_world_2d().direct_space_state
-		var end = position + (click_normalized * 100 )
-		var query = PhysicsRayQueryParameters2D.create(position, end)
-		var hit = space.intersect_ray(query)
-		print(hit)
-		
-		
-		
+	if event is InputEventMouseButton and event.button_index == 1 and event.pressed:
+		$Components/Playercontroller.handle_main_hand_action()
 
 func _physics_process(delta):
+	# Interact
+	if Input.is_action_just_pressed("player_interact"):
+		$Components/Playercontroller.handle_interact()
+	
+	if Input.is_action_just_pressed("dev_debug"):
+		$Components/Playercontroller.debug()
+	
+	# Movement
 	velocity.x = Input.get_axis("player_left", "player_right") * SPEED
 	velocity.y = Input.get_axis("player_up", "player_down") * SPEED
+
+	$Components/Animationcontroller.handle_flip(velocity)
+
+	if velocity != Vector2():
+		$Components/Animationcontroller.handle_walk_forward()
+	else:
+		$Components/Animationcontroller.handle_idle()
 
 	move_and_slide()
