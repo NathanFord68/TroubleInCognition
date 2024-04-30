e2050326d0a79d261d880ff9320ff953c405cc38
Nathan Ford <NathanFord68@gmail.com>
2024-04-30 17:43:16 -0400

    Added animation tree for animation control Updated code to leverage animation tree Split hatchet into world gen and player gen sprites Added beginnings of UI for player inventory/equipment management

--- player/player.gd
+++ player/player.gd
@@ -1,33 +1,43 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+const SPEED : float = 300.0
 
 var equipped = null
 
-func _ready():
-	$AnimationPlayer.play("Idle")
-
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == 1 and event.pressed:
-		$Components/Playercontroller.handle_main_hand_action()
+		$Playercontroller.handle_main_hand_action()
 
 func _physics_process(delta):
 	# Interact
 	if Input.is_action_just_pressed("player_interact"):
-		$Components/Playercontroller.handle_interact()
+		$Playercontroller.handle_interact()
 	
 	if Input.is_action_just_pressed("dev_debug"):
-		$Components/Playercontroller.debug()
+		$Playercontroller.debug()
+		
+	if Input.is_action_just_pressed("player_toggle_inventory"):
+		# Get size of the players vision
+		var camera_size : Vector2 = get_canvas_transform().affine_inverse().basis_xform(get_viewport_rect().size)
+		
+		# Set the parameters of the inventory screen
+		$Inventory.position = camera_size / -2
+		$Inventory.size = camera_size
+		$Inventory/SplitScreen.split_offset = camera_size.x / 2 
+		
+		# Pause game and show inventory
+		$Inventory.visible = true
+		get_tree().paused = true
 	
 	# Movement
 	velocity.x = Input.get_axis("player_left", "player_right") * SPEED
 	velocity.y = Input.get_axis("player_up", "player_down") * SPEED
 
-	$Components/Animationcontroller.handle_flip(velocity)
+	$Animationcontroller.handle_flip(velocity)
 
 	if velocity != Vector2():
-		$Components/Animationcontroller.handle_walk_forward()
+		$Animationcontroller.handle_walk_forward()
 	else:
-		$Components/Animationcontroller.handle_idle()
+		$Animationcontroller.handle_idle()
 
 	move_and_slide()
