a41e1721fc0792b7f77f30cc0a12521203b3e276
Nathan Ford <NathanFord68@gmail.com>
2024-05-02 18:00:16 -0400

    Updated animationc ontroller to use equipement. Equiping items onto player from inventory screen now

--- assets/item.gd
+++ assets/item.gd
@@ -1,7 +1,7 @@
 extends StaticBody2D
 class_name Item
 
-enum TYPE { HEAD, CHEST, LEGS, FEET, AMMO, CAPE, GLOVES, PRIMARY, SECONDARY }
+enum TYPE { MAIN, HEAD, CHEST, LEGS, FEET, AMMO, CAPE, GLOVES, PRIMARY, SECONDARY, RING }
 
 @export var damage : float
 @export var range : float
@@ -16,11 +16,6 @@ func _interact(player):
 	# Instantiate a new hatchet
 	var n_item = load(asset_path)
 	var in_item = n_item.instantiate()
-	
-	# Set up hatchet for player use
-	in_item.get_node("AnimationTree").active = true
-	in_item.get_node("WorldGen").visible = false
-	in_item.get_node("PlayerGen").visible = true
 	
 	# Add the hatchet to the inventory
 	player.get_node("InventoryManager").add_to_inventory(in_item)
