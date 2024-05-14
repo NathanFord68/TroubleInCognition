57afad5914d6862a8b4c441dbb19391bcab71e20
Nathan Ford <NathanFord68@gmail.com>
2024-05-14 14:29:14 -0400

    Added a hatchet

--- scripts/engine_info.gd
+++ scripts/engine_info.gd
@@ -6,9 +6,3 @@ class_name EngineInfo
 ## The path of this asset in the engine
 @export 
 var asset_path: String
-
-## The path of the representing icon in the engine
-@export 
-var icon_path: String
-
-const temp: String = "res://_assets/tools/hatchet/hatchet_world_gen.png"
