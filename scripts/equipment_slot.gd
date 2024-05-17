4ad504caf69e9bdf741104aa0dbdb3a2c9976228
Nathan Ford <NathanFord68@gmail.com>
2024-05-17 18:18:49 -0400

    Got the inventory icon to reflect the correct color

--- scripts/equipment_slot.gd
+++ scripts/equipment_slot.gd
@@ -29,15 +29,24 @@ func update_icon():
 		$Item.texture = null
 		return
 		
-	var final_img = Image.new()
+	var item_size : Vector2 = (item.get_node("CollisionShape2D") as CollisionShape2D).shape.get_rect().size + Vector2(6, 6)
+	var final_img = Image.create(item_size.x, item_size.y, false,Image.FORMAT_RGBA8)
+	
+	# TODO refactor this so it's not nested so deep
 	for child : Sprite2D in item.get_node("Sprite").get_children():
 		var img = child.texture.get_image()
-		img.crop(64, 64)
-		if final_img.is_empty():
-			final_img = img
-			continue 
-		final_img.blend_rect(img, Rect2(0, 0, 64, 64), Vector2(0, 0))
-
+		final_img.blend_rect(img, Rect2(
+			32 - (item_size.x / 2), 
+			32 - (item_size.y / 2), 
+			item_size.x, 
+			item_size.y), Vector2(0, 0))
+		if child.self_modulate != Color(1, 1, 1, 1):
+			for x in range(0, item_size.x):
+				for y in range(0, item_size.y):
+					if final_img.get_pixel(x, y) == Color(1, 1, 1, 1):
+						final_img.set_pixel(x, y, child.self_modulate)
+		
+	
 	$Item.texture = ImageTexture.create_from_image(final_img)
 	return	
 
@@ -65,7 +74,7 @@ func _can_drop_data(_at_position, data):
 		return false
 	return true
 	
-func _drop_data(at_position : Vector2, data : Variant):
+func _drop_data(at_position : Vector2, data : Variant) -> void:
 	# If the slot has something not allowed in our slot return
 	if ( data.allowed_type != Enums.ITEM_TYPE.MAIN 
 		and is_instance_valid(item)
